20855e541806f4aa383f37efc16517dc20cbe549
Nsankar03 <111137652+Nsankar03@users.noreply.github.com>
2022-11-02 20:37:10 +0530

    Update Kth Smallest Element in a Sorted Matrix.java

--- Homework assignments/week 7/Kth Smallest Element in a Sorted Matrix.java
+++ Homework assignments/week 7/Kth Smallest Element in a Sorted Matrix.java
@@ -1,31 +1,17 @@
 class Solution {
-    class Node{
-        int val;
-        int row;
-        int col;
-        
-        Node(int val,int row,int col){
-            this.val=val;
-            this.row=row;
-            this.col=col;
-        }
-    }
-    
     public int kthSmallest(int[][] matrix, int k) {
-        int n=matrix.length;
-        PriorityQueue<Node> q= new PriorityQueue<>(n,(a,b)->a.val-b.val);
-        
-        for(int i=0;i<n;i++){
-            q.add(new Node(matrix[i][0],i,0));
-        }
-        
-        for(int i=0;i<k-1;i++){
-            Node temp=q.poll();
-            
-            if(temp.col<n-1){
-                q.add(new Node(matrix[temp.row][temp.col+1],temp.row,temp.col+1));
+        PriorityQueue<Integer> q=new PriorityQueue<>();
+        for(int i=0;i<matrix.length;i++)
+        {
+            for(int j=0;j<matrix[0].length;j++)
+            {
+                q.offer(matrix[i][j]);
             }
         }
-        return q.poll().val;
+        for(int i=0;i<k-1;i++)
+        {
+            q.poll();
+        }
+        return q.peek();
     }
 }
